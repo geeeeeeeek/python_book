cc95efd788105add50c8885bf63cd2745a5ea252
Tim Green <lovezww2011@gmail.com>
2024-10-28 22:02:17 +0800

    Update permission.js

--- web/src/permission.js
+++ web/src/permission.js
@@ -3,7 +3,7 @@ import store from './store'
 import storage from 'store'
 import { ADMIN_TOKEN, TOKEN } from '@/store/constants'
 
-// 路由权限白名单
+// 路由权限白名单（登录或403页面）
 const allowList = ['admin-login', 'login', 'register', 'portal', 'search', 'detail', '403', '404']
 // 前台登录地址
 const loginRoutePath = '/index/login'
